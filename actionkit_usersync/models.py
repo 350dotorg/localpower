50e7ca149105f0d66dc04ec265a6d17214f576d8
Ethan Jucovy <ejucovy@gmail.com>
2011-07-31 17:48:52 -0400

    Sync user data to ActionKit when creating or modifying an account; requires actionkit API user/pw/host settings in local_settings

---
+++ actionkit_usersync/models.py
@@ -0,0 +1,53 @@
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.db import models
+from actionkit_usersync.utils import get_client
+from rah.models import Profile
+
+class null_location(object):
+    def __getattr__(self, *args, **kw):
+        return ''
+
+def actionkit_push(user, profile):
+    location = profile.location or null_location()
+    # EGJ TODO: what if profile.location is None?
+    # i think the localpower system allows that
+    # currently just setting empty strings (per line above)
+    
+    actionkit = get_client()
+    ak_user = actionkit.User.save_or_create(dict(
+            email=user.email,
+            first_name=user.first_name,
+            last_name=user.last_name,
+            zip=location.zipcode,
+            region=location.county,
+            state=location.st,
+            state_name=location.state,
+            city=location.name
+            ))
+    return ak_user
+
+def user_post_save(sender, instance, created, **kwargs):
+    user = instance
+    profile = user.get_profile()
+    ak_user = actionkit_push(user, profile)
+models.signals.post_save.connect(user_post_save, sender=User)
+
+def profile_post_save(sender, instance, created, **kwargs):
+    profile = instance
+    user = profile.user
+    ak_user = actionkit_push(user, profile)
+models.signals.post_save.connect(profile_post_save, sender=Profile)
+
+# EGJ TODO: test profile post-save signal
+
+# Verify that the ActionKit API settings are correct 
+# and successfully connect to the server
+actionkit = get_client()
+import xmlrpclib
+try:
+    actionkit.version()
+except xmlrpclib.ProtocolError:
+    print ("Your ActionKit API settings are incorrect; please double-check them "
+           "and try again.")
+    raise
