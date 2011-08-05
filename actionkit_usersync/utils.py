237349dbc0fe0b5bcd26405d404836f8e2d0e493
Ethan Jucovy <ejucovy@gmail.com>
2011-08-05 08:43:15 -0400

    allow user to specify an https:// url for ACTIONKIT_API_HOST

--- actionkit_usersync/utils.py
+++ actionkit_usersync/utils.py
@@ -1,9 +1,17 @@
 from django.conf import settings
+from urlparse import urlparse
 import xmlrpclib
 
 def get_client():
-    actionkit = xmlrpclib.Server('http://%s:%s@%s/api/' % (
+    host = settings.ACTIONKIT_API_HOST
+    if not host.startswith("http"):
+        host = "http://" + host
+    host = urlparse(host)
+    url = '%s://%s:%s@%s/api/' % (
+            host.scheme,
             settings.ACTIONKIT_API_USER,
             settings.ACTIONKIT_API_PASSWORD,
-            settings.ACTIONKIT_API_HOST))
+            host.netloc)
+    print url
+    actionkit = xmlrpclib.Server(url)
     return actionkit
