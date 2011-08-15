db2b227c48330ee2fd7a5270b66020bf3e4e7636
Ethan Jucovy <ejucovy@gmail.com>
2011-08-15 08:38:37 -0400

    redirect to user profile url instead of showing it directly to logged in users on homepage, so that we are using canonical urls as much as possible

--- rah/views.py
+++ rah/views.py
@@ -91,7 +91,7 @@ def index(request):
     """
     # If the user is not logged in, show them the logged out homepage and bail
     if request.user.is_authenticated():
-        return profile(request, request.user.id)
+        return redirect("profile", user_id=request.user.id) 
 
     section_class = "section_home"
     top_users = Profile.objects.filter(is_profile_private=False, location__isnull=False, facebook_connect_only=True).select_related("user").order_by("-total_points")[:6]
