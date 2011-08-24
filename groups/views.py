4cb813aa02101bf4822c63902ade4365e6470803
Ethan Jucovy <ejucovy@gmail.com>
2011-08-23 21:55:32 -0400

    groups map widget

--- groups/views.py
+++ groups/views.py
@@ -188,6 +188,7 @@ def group_list(request):
     groups = Group.objects.groups_with_memberships(request.user)
     if request.user.is_authenticated():
         my_groups = Group.objects.filter(users=request.user, is_geo_group=False)
+    map_events = Group.objects.filter(geom__isnull=False)
     return render_to_response("groups/group_list.html", locals(), 
                               context_instance=RequestContext(request))
 
