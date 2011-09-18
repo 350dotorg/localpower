8680d61456b5960654d23510a8a849298a0e8f59
Ethan Jucovy <ejucovy@gmail.com>
2011-09-18 08:38:21 -0400

    port actionkit_usersync to geoms by parsing google location and attempting to corrolate all the fields with actionkit's america-centric data model. some of these appear to be discarded on the actionkit end for being non-USA, like state_name and perhaps even region. but my code is right, for the day when actionkit stops being US-centric.

--- actionkit_usersync/models.py
+++ actionkit_usersync/models.py
@@ -3,27 +3,115 @@ from django.contrib.auth.models import User
 from django.db import models
 from actionkit_usersync.utils import get_client
 from rah.models import Profile
+from geo.fields import GoogleGeoField
 
 class null_location(object):
     def __getattr__(self, *args, **kw):
         return ''
 
+class google_location(object):
+    def __init__(self, geom):
+        self.g = GoogleGeoField().clean(geom.formatted_address)
+        addr = dict()
+        for row in self.g['google_top_result']['address_components']:
+            addr[tuple(row['types'])] = row
+            for i in row['types']:
+                addr[i] = row
+        self.addr = addr
+
+    @property
+    def postal(self):
+        try:
+            postal = self.addr['postal']
+        except:
+            return ''
+        return postal['long_name']
+
+    @property
+    def zipcode(self):
+        if self.country != "United States":
+            return ''
+        return self.postal
+
+    @property
+    def county(self):
+        try:
+            addr = self.addr['administrative_area_level_2']
+        except:
+            pass
+        else:
+            return addr['long_name']
+        try:
+            addr = self.addr['administrative_area_level_1']
+        except:
+            return ''
+        return addr['long_name']
+
+    @property
+    def st(self):
+        if self.country != "United States":
+            return ''
+        try:
+            addr = self.addr['administrative_area_level_1']
+        except:
+            return ''
+        return addr['short_name']
+
+    @property
+    def state(self):
+        try:
+            addr = self.addr['administrative_area_level_1']
+        except:
+            return ''
+        return addr['long_name']
+
+    @property
+    def name(self):
+        try:
+            addr = self.addr['sublocality']
+        except:
+            pass
+        else:
+            return addr['long_name']
+        try:
+            addr = self.addr['locality']
+        except:
+            return ''
+        else:
+            return addr['long_name']
+
+    @property
+    def country(self):
+        try:
+            addr = self.addr['country']
+        except:
+            return ''
+        else:
+            return addr['long_name']
+        
 def actionkit_push(user, profile):
-    location = profile.location or null_location()
-    # EGJ TODO: what if profile.location is None?
+    geom = profile.geom
+    if geom:
+        location = google_location(geom)
+    else:
+        location = null_location()
+
+    # EGJ TODO: what if profile.geom is None?
     # i think the localpower system allows that
     # currently just setting empty strings (per line above)
-    
+   
     actionkit = get_client()
     ak_user = actionkit.User.save_or_create(dict(
             email=user.email,
             first_name=user.first_name,
             last_name=user.last_name,
             zip=location.zipcode,
+            postal=location.postal,
             region=location.county,
             state=location.st,
             state_name=location.state,
-            city=location.name
+            city=location.name,
+            country=location.country
             ))
 
     try:
