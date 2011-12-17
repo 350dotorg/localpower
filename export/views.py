c119756bde40d690a819871fdbc049cd93f8b3da
Ethan Jucovy <ejucovy@gmail.com>
2011-12-17 13:34:00 -0500

    when exporting users, send each user an email explaining that an account has been created and containing a magic link that will let him set a new password and then log him in for the first time

--- export/views.py
+++ export/views.py
@@ -2,6 +2,7 @@ import csv
 
 from django.contrib import messages
 from django.contrib.admin.views.decorators import staff_member_required
+from django.contrib.auth.forms import PasswordResetForm
 from django.contrib.auth.models import UNUSABLE_PASSWORD
 from django.db import transaction
 from django.http import HttpResponse, HttpResponseRedirect
@@ -82,7 +83,15 @@ def _import_users(request):
                         user.pk, user.get_full_name(), group.pk, group.name))
             else:
                 message = ("Added user: <a href='/admin/auth/user/%s/'>%s</a> " % (
-                        user.pk, user.get_full_name(), group.pk, group.name))
+                        user.pk, user.get_full_name()))
+
+            ## Now send the new user an email, telling him we created an account
+            ## and giving him a link to set a password for his account.
+            form = PasswordResetForm({'email': user.email})
+            if form.is_valid():
+                form.save(email_template_name='export/user_import_password_reset_email.html',
+                          use_https=request.is_secure(),
+                          request=request)
 
             messages.success(request, message)
 
@@ -127,3 +136,69 @@ def user_import(request):
     groups = Group.objects.all()
 
     return render_to_response("export/user_import_preview.html", locals(), context_instance=RequestContext(request))
+
+import re
+from django.conf import settings
+from django.contrib.auth import REDIRECT_FIELD_NAME
+# Avoid shadowing the login() view below.
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import authenticate
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import PasswordResetForm, SetPasswordForm, PasswordChangeForm
+from django.contrib.auth.tokens import default_token_generator
+from django.views.decorators.csrf import csrf_protect
+from django.core.urlresolvers import reverse
+from django.shortcuts import render_to_response, get_object_or_404
+from django.contrib.sites.models import get_current_site
+from django.http import HttpResponseRedirect, Http404
+from django.template import RequestContext
+from django.utils.http import urlquote, base36_to_int
+from django.utils.translation import ugettext as _
+from django.contrib.auth.models import User
+from django.views.decorators.cache import never_cache
+
+@never_cache
+def password_reset_confirm(request, uidb36=None, token=None, template_name='registration/password_reset_confirm.html',
+                           token_generator=default_token_generator, set_password_form=SetPasswordForm,
+                           post_reset_redirect=None):
+    """
+    View that checks the hash in a password reset link and presents a
+    form for entering a new password.
+    """
+    assert uidb36 is not None and token is not None # checked by URLconf
+    if post_reset_redirect is None:
+        post_reset_redirect = reverse('django.contrib.auth.views.password_reset_complete')
+    try:
+        uid_int = base36_to_int(uidb36)
+        user = User.objects.get(id=uid_int)
+    except (ValueError, User.DoesNotExist):
+        user = None
+
+    context_instance = RequestContext(request)
+
+    if user is not None and token_generator.check_token(user, token):
+        context_instance['validlink'] = True
+        if request.method == 'POST':
+            form = set_password_form(user, request.POST)
+            if form.is_valid():
+                form.save()
+                user_with_backend = authenticate(username=user.username, 
+                                                 password=form.cleaned_data['new_password1'])
+                auth_login(request, user_with_backend)
+                return HttpResponseRedirect(post_reset_redirect)
+        else:
+            form = set_password_form(None)
+    else:
+        context_instance['validlink'] = False
+        form = None
+    context_instance['form'] = form
+    context_instance['user'] = user
+    return render_to_response(template_name, context_instance=context_instance)
+
+
+def account_claim_complete(request):
+    messages.success(request,
+                     _("Your password has been set, and you are now logged in.  Welcome!"),
+                     extra_tags="sticky")
+    return redirect("index")
