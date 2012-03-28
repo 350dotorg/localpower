64a5dfcad4d080998c4dd142c14d6bae247c18c3
Ethan Jucovy <ejucovy@gmail.com>
2012-03-28 14:10:48 -0400

    clear migrations

--- migrations/migrate.sql
+++
@@ -1,5 +0,0 @@
-alter table discussions_discussion add column extra_disambiguator varchar(100);
-alter table discussions_discussion modify column user_id integer;
-alter table discussions_discussion add column mock_user longtext;
-
-alter table actionkit_account add column `random_string` varchar(100);
